99f8b434712e006fc4b4c1431e070a2eb3720d83
yuhaiping <yuhaiping@terminus.io>
2018-04-11 10:18:34 +0800

    add usage

---
+++ zk/usage/src/main/main.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main()  {
+	fmt.Println("Hello Golang")
+
+	// 服务器地址列表
+	servers := []string{"192.168.3.2:2181", "192.168.3.2:2182", "192.168.3.2:2183"}
+	client, err := NewClient(servers, "/api", 10)
+	if err != nil {
+		panic(err)
+	}
+	defer client.Close()
+	node1 := &ServiceNode{"user", "127.0.0.1", 4000}
+	node2 := &ServiceNode{"user", "127.0.0.1", 4001}
+	node3 := &ServiceNode{"user", "127.0.0.1", 4002}
+	if err := client.Register(node1); err != nil {
+		panic(err)
+	}
+	if err := client.Register(node2); err != nil {
+		panic(err)
+	}
+	if err := client.Register(node3); err != nil {
+		panic(err)
+	}
+	nodes, err := client.GetNodes("user")
+	if err != nil {
+		panic(err)
+	}
+	for _, node := range nodes {
+		fmt.Println(node.Host, node.Port)
+	}
+
+}
