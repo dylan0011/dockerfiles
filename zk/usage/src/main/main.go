173ae50a9725fd32f012700aab5b564e9e5ed17c
yuhaiping <yuhaiping@terminus.io>
2018-07-04 14:57:23 +0800

    do something i do not know

--- zk/usage/src/main/main.go
+++ zk/usage/src/main/main.go
@@ -9,14 +9,16 @@ func main()  {
 
 	// 服务器地址列表
 	servers := []string{"192.168.3.2:2181", "192.168.3.2:2182", "192.168.3.2:2183"}
+
+	println(servers)
 	client, err := NewClient(servers, "/api", 10)
 	if err != nil {
 		panic(err)
 	}
 	defer client.Close()
-	node1 := &ServiceNode{"user", "127.0.0.1", 4000}
-	node2 := &ServiceNode{"user", "127.0.0.1", 4001}
-	node3 := &ServiceNode{"user", "127.0.0.1", 4002}
+	node1 := &ServiceNode{"user", "192.168.3.2", 4000}
+	node2 := &ServiceNode{"user", "192.168.3.2", 4001}
+	node3 := &ServiceNode{"user", "192.168.3.2", 4002}
 	if err := client.Register(node1); err != nil {
 		panic(err)
 	}
